7562ca7c85b04720c166f4008a412e2152d35dae
abc <abc@abc.com>
2016-11-16 18:33:12 -0800

    intial commit

---
+++ week6/arguments.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is for arguments - Yay!
+# Call this script with:
+# arguments.sh <first> <second>
+
+echo "=======Argument Examples======="
+echo $1
+echo "$2 is the second argument"
+
+echo "You made some good arguments there, I will not tolerate any more"
+
+echo "I lied, here they are:"
+echo $5 $3 $8 $4 $7 $1 $2
+
+
---
+++ week6/variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This scipt will work with variables
+
+VAR1="Some text"
+WEATHER="damp"
+DAYS=2
+
+echo "=======Variable Examples======="
+echo "The weath is $WEATHER and has been this way for $DAYS day(s)"
